4828cd13c9cc164c4b34e2d43ce9db37c9e6524d
Surya <suryak22@outlook.com>
2016-09-09 12:27:15 +0530

    Full Featured user admin python web app for movie with sqlite

--- movie/urls.py
+++ movie/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<requester>[\w\-]+)/$', views.check_requester, name='requester'),
     url(r'^(?P<requester>[\w\-]+)/registry/$', views.movie_registry, name='registry'),
-    url(r'^details/(?P<movie_id>[0-9]+)/(?P<requester>[\w\-]+)/$', views.movie_details, name='details'),
+    url(r'^(?P<requester>[\w\-]+)/details/(?P<movie_id>[0-9]+)/$', views.movie_details, name='details'),
     url(r'^remove/(?P<movie_id>[0-9]+)$', views.remove_movie, name='remove_movie'),
-    url(r'^like/(?P<movie_id>[0-9]+)$', views.like_movie, name='like_movie'),
-    url(r'^unlike/(?P<movie_id>[0-9]+)$', views.unlike_movie, name='unlike_movie'),
+    url(r'^update/(?P<movie_id>[0-9]+)$', views.update_movie, name='update'),
+
+    url(r'^like/(?P<requester>[\w\-]+)/(?P<movie_id>[0-9]+)$', views.like_movie, name='like_movie'),
+    url(r'^unlike/(?P<requester>[\w\-]+)/(?P<movie_id>[0-9]+)$', views.unlike_movie, name='unlike_movie'),
 
     # url(r'^(?P<requester>[\w\-]+)/$', views.remove_movie, name='requester_page'),
 
--- movie/views.py
+++ movie/views.py
@@ -9,10 +9,10 @@ def index(request):
     return render(request, 'movie/index.html')
 
 
-def check_requester(request,requester):
+def check_requester(request, requester):
     if request.method == 'GET':
-        return render(request,'movie/movie_list.html', {'requester': requester,'movie_list' : Movie.objects.all(),
-                                                        'rating': Rating})
+        return render(request, 'movie/movie_list.html', {'requester': requester,'movie_list': Movie.objects.all(),
+                                                         'rating': Rating})
 
 
 def movie_registry(request, requester):
@@ -26,45 +26,58 @@ def movie_registry(request, requester):
             movie_producer = form.cleaned_data['movie_producer']
             movie_cast_actor = form.cleaned_data['movie_cast_actor']
             movie_cinematography = form.cleaned_data['movie_cinematography']
-            movie_obj = Movie.objects.create(name_of_movie = name_of_movie, movie_director = movie_director,
-                                             movie_producer= movie_producer, movie_cast_actor = movie_cast_actor,
-                                             movie_cinematography = movie_cinematography)
-            Rating.objects.create(movie=movie_obj,up_vote_count=0,down_vote_count=0)
+            movie_obj = Movie.objects.create(name_of_movie=name_of_movie, movie_director=movie_director,
+                                             movie_producer=movie_producer, movie_cast_actor=movie_cast_actor,
+                                             movie_cinematography=movie_cinematography)
+            Rating.objects.create(movie=movie_obj, up_vote_count=0, down_vote_count=0)
 
             return HttpResponseRedirect(reverse('index'))
-    return render(request, 'movie/movie_registry.html', {'form': form, 'requester':requester})
+    return render(request, 'movie/movie_registry.html', {'form': form, 'requester': requester})
 
 
 def movie_details(request, movie_id, requester):
-    # import ipdb;ipdb.set_trace()
     movie_instance = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movie/movie_details.html', {'movie': movie_instance, 'requester': requester})
 
 
 def remove_movie(request, movie_id):
     get_object_or_404(Movie, pk=movie_id).delete()
-    # Movie.objects.filter(pk=movie_id).delete()
-    return render(request, 'movie/movie_list.html', {'requester': False, 'movie_list' : Movie.objects.all()})
+    return render(request, 'movie/movie_list.html', {'requester': False, 'movie_list': Movie.objects.all()})
 
 
-def like_movie(request, movie_id):
-    import ipdb;ipdb.set_trace()
-    movie = get_object_or_404(Movie,pk=movie_id)
-    rating_inst=movie.rating.up_vote_count+1
-    return  reverse('details', kwargs={'movie_id': movie_id, 'requester': 'AnonymousUser'})
-
-
-def unlike_movie(request, movie_id):
+def like_movie(request, requester, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
-    rating_inst = movie.rating.up_vote_count + 1
-    return render(request, 'movie/movie_details.html', {'movie': movie_id, 'requester': 'AnonymousUser'})
+    rating_instance=movie.rating
+    rating_instance.up_vote_count = rating_instance.up_vote_count + 1
+    rating_instance.save()
+    return HttpResponseRedirect(reverse('details', kwargs={'movie_id': movie_id, 'requester': requester}))
+
+
+def unlike_movie(request, requester, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    rating_instance = movie.rating
+    rating_instance.down_vote_count += 1
+    rating_instance.save()
+    return HttpResponseRedirect(reverse('details', kwargs={'movie_id': movie_id, 'requester': requester}))
 
 
 def update_movie(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
     if request.method == 'GET':
-        movie = get_object_or_404(Movie, pk=movie_id)
         data = {'name_of_movie': movie.name_of_movie, 'movie_director': movie.movie_director,
                 'movie_producer': movie.movie_producer, 'movie_cast_actor': movie.movie_cast_actor,
                 'movie_cinematography': movie.movie_cinematography}
         form = MovieDetails(data)
-        return render(request, 'movie/movie_registry.html', {'form': form, 'requester': 'AnonymousUser'})
+    else:
+        form = MovieDetails(request.POST)
+        if form.is_valid():
+            name_of_movie = form.cleaned_data['name_of_movie']
+            movie_director = form.cleaned_data['movie_director']
+            movie_producer = form.cleaned_data['movie_producer']
+            movie_cast_actor = form.cleaned_data['movie_cast_actor']
+            movie_cinematography = form.cleaned_data['movie_cinematography']
+            Movie.objects.filter(pk=movie_id).update(name_of_movie=name_of_movie, movie_director=movie_director,
+                                 movie_producer=movie_producer, movie_cast_actor=movie_cast_actor,
+                                 movie_cinematography=movie_cinematography)
+            return HttpResponseRedirect(reverse('details', kwargs={'movie_id': movie_id, 'requester': 'admin'}))
+    return render(request, 'movie/update_movie.html', {'movie_id': movie_id, 'form': form})
